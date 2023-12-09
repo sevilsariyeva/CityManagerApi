3f150741e5aa343f8d2bcba7e283f710aaeb96de
sevilsariyeva00 <sevilsariyeva00@gmail.com>
2023-12-09 19:48:09 +0400

    .

--- CityManagerApi/Program.cs
+++ CityManagerApi/Program.cs
@@ -2,24 +2,18 @@ using CityManagerApi.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddControllers()
     .AddJsonOptions(opt =>
     {
         opt.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
     });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
 
 builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
 {
@@ -48,21 +42,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 });
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-    app.UseStaticFiles(new StaticFileOptions
-    {
-        FileProvider = new PhysicalFileProvider(
-            Path.Combine(Directory.GetCurrentDirectory(), "StaticFiles")),
-        RequestPath = "/StaticFiles"
-    });
-}
 
 app.UseHttpsRedirection();
 
@@ -71,5 +51,8 @@ app.UseCors("corsapp");
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseDefaultFiles();
+app.UseStaticFiles(); 
 
 app.Run();
